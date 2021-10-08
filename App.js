397a0b07977c7e05b792289eaab76ce5f578f0d8
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-10-08 10:55:40 -0400

    fri

--- App.js
+++ App.js
@@ -1,26 +1,59 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+} from "react-native";
+import PropTypes from "prop-types";
 import { Directions } from "react-native-gesture-handler";
 import { set } from "react-native-reanimated";
 
-function useTimer(init) {
-  const [counter, setCounter] = React.useState(init);
+const myData = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+const Item = ({ year, month, day, hour, minute }) => (
+  <View>
+    <Text style={styles.title}>
+      {year}-{month}-{day} {hour}:{minute}
+    </Text>
+  </View>
+);
+
+function useTimer(myCycle, mySetTime, myRestTime) {
+  const [counter, setCounter] = React.useState(() => {
+    return mySetTime;
+  });
   const [running, setRunning] = React.useState(false);
   const [cycle, setCycle] = React.useState(1);
   const [rest, setRest] = React.useState(false);
+  let workOutData = [];
 
   const reset = React.useRef();
-  reset.current = init;
-
-  // const currentCycle = React.useRef(cycle)
-  // const prevCycle = React.useRef(0)
+  reset.current = mySetTime;
 
   let myTimer = React.useRef();
 
   React.useEffect(() => {
-    // console.log("effect start", counter, running);
-    if (cycle == 3) {
+    console.log(myCycle, mySetTime, myRestTime);
+    if (cycle == myCycle + 1) {
       stop();
     }
     if (running) {
@@ -33,98 +66,204 @@ function useTimer(init) {
         }, 1000);
       } else {
         if (rest) {
-          setCounter(10);
+          setCounter(myRestTime);
         } else {
-          setCounter(20);
+          setCounter(mySetTime);
           setCycle(cycle + 1);
         }
       }
     }
     return () => {
-      // console.log("effect finish", counter, running);
       clearInterval(myTimer.current);
+      logDate(workOutData);
     };
-  }, [counter]);
+  }, [counter, myCycle, mySetTime, myRestTime]);
+
+  const logDate = (arr) => {
+    const thisDate = new Date();
+    arr.push({
+      year: thisDate.getFullYear(),
+      month: thisDate.getMonth() + 1,
+      day: thisDate.getDate(),
+      hour: thisDate.getHours(),
+      minute: thisDate.getMinutes(),
+    });
+  };
 
   const start = () => {
     setCounter(counter - 1);
     setRunning(true);
+    logDate(workOutData);
   };
+  logDate(workOutData);
   const stop = () => {
     // clearInterval(myTimer.current);
     setCounter(reset.current);
     setRunning(false);
     setRest(false);
     setCycle(1);
+    logDate(workOutData);
   };
 
-  return { counter, setCounter, start, stop, rest, cycle, running };
+  return {
+    counter,
+    setCounter,
+    start,
+    stop,
+    rest,
+    cycle,
+    setCycle,
+    running,
+    workOutData,
+  };
 }
 
 export default function App() {
-  const { counter, setCounter, start, stop, rest, cycle, running } =
-    useTimer(20);
+  const [myCycle, setMyCycle] = React.useState(10);
+  const [mySetTime, setMySetTime] = React.useState(20);
+  const [myRestTime, setMyRestTime] = React.useState(10);
+
+  const {
+    counter,
+    setCounter,
+    start,
+    stop,
+    rest,
+    cycle,
+    setCycle,
+    running,
+    workOutData,
+  } = useTimer(myCycle, mySetTime, myRestTime);
+
+  const renderItem = ({ item }) => (
+    <Item
+      year={item.year}
+      month={item.month}
+      day={item.day}
+      hour={item.hour}
+      minute={item.minute}
+    />
+  );
 
   return (
-    <View
-      style={
-        !rest && running
-          ? styles.container2
-          : running && rest
-          ? styles.container3
-          : styles.container
-      }
-    >
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Workout </Text>
-        <Text style={styles.headerText}> App </Text>
-      </View>
-      <View style={styles.counter}>{counter}</View>
-      <View style={styles.buttonView}>
-        {!running ? (
-          <TouchableOpacity style={styles.button} onPress={start}>
-            {" "}
-            Start{" "}
-          </TouchableOpacity>
-        ) : null}
+    <SafeAreaView>
+      <View
+        style={
+          !rest && running
+            ? styles.container2
+            : running && rest
+            ? styles.container3
+            : styles.container
+        }
+      >
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Menu </Text>
+          <Image
+            style={styles.logo}
+            source={require("./images/logo/logo-05.png")}
+          />
+          <Text style={styles.headerText}> Profile </Text>
+        </View>
+        <View style={styles.inputsView}>
+          <TextInput
+            style={styles.input}
+            placeholder="Cycles"
+            onChangeText={(x) => {
+              if (+x) {
+                setMyCycle(+x);
+                // setCycle(+x);
+              }
+            }}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Time for a set"
+            onChangeText={(x) => {
+              if (+x) {
+                setMySetTime(+x);
+                setCounter(+x);
+              }
+            }}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Rest Time"
+            onChangeText={(x) => {
+              if (+x) {
+                setMyRestTime(+x);
+              }
+            }}
+          />
+        </View>
+        <View style={styles.counter}>{counter}</View>
+        <View style={styles.buttonView}>
+          {!running ? (
+            <TouchableOpacity style={styles.button} onPress={start}>
+              {" "}
+              Start{" "}
+            </TouchableOpacity>
+          ) : null}
+          {running ? (
+            <TouchableOpacity style={styles.button} onPress={stop}>
+              {" "}
+              Stop{" "}
+            </TouchableOpacity>
+          ) : null}
+        </View>
+        {running && rest ? (
+          <Text style={styles.paragraph1}>Resting 🛌</Text>
+        ) : running && !rest ? (
+          <Text style={styles.paragraph1}>Working Out 🏋️‍♀️</Text>
+        ) : (
+          <Text style={styles.paragraph1}>Ready For Workout?</Text>
+        )}
         {running ? (
-          <TouchableOpacity style={styles.button} onPress={stop}>
-            {" "}
-            Stop{" "}
-          </TouchableOpacity>
-        ) : null}
+          <Text style={styles.paragraph2}>Cycle: {cycle}</Text>
+        ) : (
+          <Text style={styles.paragraph2}>{myCycle} Cycles</Text>
+        )}
+        <SafeAreaView>
+          <FlatList
+            data={workOutData}
+            renderItem={renderItem}
+            // keyExtractor={(item) => item.id}
+          />
+        </SafeAreaView>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Copyright © 2021 Mintes, Inc.</Text>
+        </View>
       </View>
-      {running && rest ? (
-        <Text style={styles.paragraph1}>Resting 🛌</Text>
-      ) : running && !rest ? (
-        <Text style={styles.paragraph1}>Working Out 🏋️‍♀️</Text>
-      ) : (
-        <Text style={styles.paragraph1}>Ready For Workout?</Text>
-      )}
-      {running ? (
-        <Text style={styles.paragraph2}>Cycle: {cycle}</Text>
-      ) : (
-        <Text style={styles.paragraph2}>10 Cycles</Text>
-      )}
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>by Mintes </Text>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputsView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 60,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
   headerText: {
     fontFamily: "Helvetica",
-    fontSize: 30,
+    fontSize: 18,
     color: "#C6D57E",
   },
   header: {
-    height: 80,
+    height: 120,
     borderColor: "black",
     backgroundColor: "black",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
     flexDirection: "row",
     margin: 0,
     padding: 20,
@@ -148,7 +287,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     // paddingTop: Constants.statusBarHeight,
     backgroundColor: "#ecf0f1",
-    padding: 8,
+    // padding: 8,
   },
   container2: {
     flex: 1,
@@ -197,5 +336,14 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 1,
     width: 200,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
